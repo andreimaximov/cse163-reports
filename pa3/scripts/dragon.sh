7346eec3a32e8d726b549dc45240162d259291d5
Andrei Maximov <andrei.maximov@gmail.com>
2017-12-17 23:37:30 -0800

    Add dragon mesh to repo

--- pa3/scripts/dragon.sh
+++ pa3/scripts/dragon.sh
@@ -4,13 +4,4 @@ SCRIPTDIR=$(cd $(dirname ${0}) && pwd -P)
 
 source ${SCRIPTDIR}/env.sh
 
-OBJ="${CACHE}/CornellBox-Dragon.obj"
-URL="http://andreimaximov.com/files/projects/pathtracer/CornellBox-Dragon.obj"
-
-if [[ ! -f ${OBJ} || ! -d ${CACHE} ]]; then
-    echo "Downloading OBJ file..."
-    mkdir -p ${CACHE}
-    curl -o ${OBJ} ${URL}
-fi
-
-${EXE} ${OBJ} ${SCENES} "${CONFIG}/cornell-box-dragon.json"
+${EXE} "${SCENES}/CornellBox-Dragon.obj" ${SCENES} "${CONFIG}/cornell-box-dragon.json"
